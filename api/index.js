bb65308fefe0119b4a5fd3d955c4f34d32d39f64
gilang sandhika putra <sssandhika@gmail.com>
2024-08-18 09:38:19 +0700

    add react

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/api', (req, res) => {
+  res.json({ message: "Hello from the server!" });
+});
+
+// All other requests not handled will return the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
